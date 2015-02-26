385a05c980c30dd9ebab67f84864ea702db17308
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-02-26 22:30:13 +0200

    Converting project to WinForms

--- NeuralNetworksFiles/MainWindow.cs
+++ NeuralNetworksFiles/MainWindow.cs
@@ -1,11 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows.Forms;
 
 namespace NeuralNetworks
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Form
 	{
 		public MainWindow()
 		{
